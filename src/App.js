c07d4f5d38c310493204becfc0475da1c168c43e
lauticouget <lauticouget@gmail.com>
2020-11-29 23:44:02 -0300

    master | Lesson 78. Refactor Button folder structure and implementations. Functions Component created. Numbers Component created.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Button from "./components/Button";
+import Functions from "./components/Functions";
+import Numbers from "./components/Numbers";
 import MathOperations from "./components/MathOperations";
 import Result from "./components/Result";
 import "./App.css";
@@ -12,22 +14,13 @@ const App = () => {
   return (
     <main className="react-calculator">
       <Result value={undefined} />
-      <div className="numbers">
-        <Button text="1" clickHandler={clickHandlerFunction} />
-        <button>2</button>
-        <button>3</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>0</button>
-      </div>
-      <div className="functions">
-        <button>clear</button>
-        <button>r</button>
-      </div>
+      <Numbers onClickNumber={number => {
+        console.log("number:", number);
+      }}/>
+      <Functions
+        onContentClear={() => console.log("content clear")}
+        onDelete={() => console.log("on delete")}
+      />
       <MathOperations
         onClickOperation={(operation) => console.log("Operation: ", operation)}
         onClickEqual={(equal) => console.log("Equal: ", equal)}
